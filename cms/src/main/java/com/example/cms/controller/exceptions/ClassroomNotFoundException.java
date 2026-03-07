c8783e588e421d33c2ea8b9b13a4d9c55d734bfa
TrinE15 <trinity.edl@mail.utoronto.ca>
2026-03-07 17:44:06 -0500

    Add files via upload

---
+++ cms/src/main/java/com/example/cms/controller/exceptions/ClassroomNotFoundException.java
@@ -0,0 +1,6 @@
+package com.example.cms.controller.exceptions;
+public class ClassroomNotFoundException extends RuntimeException{
+    public ClassroomNotFoundException(String code) {
+        super("Could not find classroom " + code);
+    }
+}

0a4236e8cbdb566a484fa5c34f6f5ec6899366f8
edltrini <trinity.edl@mail.utoronto.ca>
2026-03-09 16:39:36 -0400

    Backend Test

--- cms/src/main/java/com/example/cms/model/repository/OrderItemRepository.java
+++ cms/src/main/java/com/example/cms/model/repository/OrderItemRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SupplierRepository extends JpaRepository<OrderItem, Integer> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
 }
